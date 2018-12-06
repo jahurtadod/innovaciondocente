a5fdc0bde3e9aad7ea7679fc6d8c1904677756f9
baesparza <bruno.be81@gmail.com>
2018-12-06 14:24:34 -0500

    feat(img modal): add function

--- components/utils/ImageModal.vue
+++ components/utils/ImageModal.vue
@@ -1,57 +1,48 @@
 <template>
   <div>
-    <img id="myImg"
+    <img id="img"
          :src="img"
          alt="Northern Lights, Norway"
-         class="img">
+         class="img"
+         v-on:click='openImage'>
 
-    <!-- The Modal -->
-    <div id="myModal"
-         class="modal">
-      <span class="close">×</span>
-      <img class="modal-content"
-           id="img01">
-      <div id="caption"></div>
+    <!-- image Modal -->
+    <div class="modal"
+         ref="modal"
+         @keyup.esc="close"
+         @click="close">
+      <span class="modal-close"
+            ref="close"
+            @click="close">×</span>
+      <img :src="img"
+           class="modal-img"
+           id="img01"
+           ref='modalImg'>
     </div>
   </div>
 </template>
 
 <script>
-// https://www.w3schools.com/css/tryit.asp?filename=trycss_image_modal_js
-/**
- // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
- */
 export default {
   data() {
     return {
       img:
         "https://about.canva.com/wp-content/uploads/sites/3/2015/01/timeline_infographic.png"
     };
+  },
+  methods: {
+    openImage() {
+      this.$refs.modal.style.display = "block";
+    },
+    close() {
+      this.$refs.modal.style.display = "none";
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-#myImg {
+.img {
   width: auto;
   border-radius: 5px;
   cursor: pointer;
@@ -62,7 +53,7 @@ export default {
   margin-left: auto;
 }
 
-#myImg:hover {
+.img:hover {
   opacity: 0.7;
 }
 
@@ -70,7 +61,7 @@ export default {
 .modal {
   display: none; /* Hidden by default */
   position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
+  z-index: 2000; /* Sit on top */
   padding-top: 100px; /* Location of the box */
   left: 0;
   top: 0;
@@ -79,37 +70,39 @@ export default {
   overflow: auto; /* Enable scroll if needed */
   background-color: rgb(0, 0, 0); /* Fallback color */
   background-color: rgba(0, 0, 0, 0.9); /* Black w/ opacity */
+  cursor: url('~/static/close.svg'), pointer;
+  &-img {
+    cursor: auto;
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    @media only screen and (max-width: 700px) {
+      & {
+        width: 100%;
+      }
+    }
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+  }
+  &-close {
+    position: absolute;
+    top: 15px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+    &:hover,
+    &:focus {
+      color: #bbb;
+      text-decoration: none;
+      cursor: pointer;
+    }
+  }
 }
-
-/* Modal Content (image) */
-.modal-content {
-  margin: auto;
-  display: block;
-  width: 80%;
-  max-width: 700px;
-}
-
-/* Caption of Modal Image */
-#caption {
-  margin: auto;
-  display: block;
-  width: 80%;
-  max-width: 700px;
-  text-align: center;
-  color: #ccc;
-  padding: 10px 0;
-  height: 150px;
-}
-
-/* Add Animation */
-.modal-content,
-#caption {
-  -webkit-animation-name: zoom;
-  -webkit-animation-duration: 0.6s;
-  animation-name: zoom;
-  animation-duration: 0.6s;
-}
-
 @-webkit-keyframes zoom {
   from {
     -webkit-transform: scale(0);
@@ -125,31 +118,6 @@ export default {
   }
   to {
     transform: scale(1);
-  }
-}
-
-/* The Close Button */
-.close {
-  position: absolute;
-  top: 15px;
-  right: 35px;
-  color: #f1f1f1;
-  font-size: 40px;
-  font-weight: bold;
-  transition: 0.3s;
-}
-
-.close:hover,
-.close:focus {
-  color: #bbb;
-  text-decoration: none;
-  cursor: pointer;
-}
-
-/* 100% Image Width on Smaller Screens */
-@media only screen and (max-width: 700px) {
-  .modal-content {
-    width: 100%;
   }
 }
 </style>
