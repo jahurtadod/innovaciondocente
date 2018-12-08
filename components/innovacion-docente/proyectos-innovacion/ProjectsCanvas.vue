9aa69046518cf549f8088d29c22e928a252672a0
baesparza <bruno.be81@gmail.com>
2018-12-08 18:46:32 -0500

    feat(canvas): add new animation

--- components/innovacion-docente/proyectos-innovacion/ProjectsCanvas.vue
+++ components/innovacion-docente/proyectos-innovacion/ProjectsCanvas.vue
@@ -15,6 +15,8 @@ export default {
       context: null,
       width: null,
       height: null,
+      center: null,
+      radius: null,
       circles: []
     };
   },
@@ -22,89 +24,42 @@ export default {
     // init canvas
     this.canvas = this.$refs.canvas;
     this.context = this.canvas.getContext("2d");
-    this.width = this.canvas.width = window.innerWidth;
-    this.height = this.canvas.height = window.innerHeight;
+    this.size = this.canvas.width = this.canvas.height = 720;
+    this.radius = {
+      min: 340 / 2,
+      max: this.size / 2
+    };
+    this.center = this.size / 2;
     this.init();
-    this.canvas.addEventListener("mousedown", this.selectProyect);
     window.requestAnimationFrame(() => this.animate());
   },
   methods: {
-    selectProyect(event) {
-      // get mouse position
-      let rect = this.canvas.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
-
-      // get if mouse touches circe
-      for (let i = 0; i < this.circles.length; i++) {
-        const circle = this.circles[i];
-        if (
-          this.getDistance(x, y, circle.position.x, circle.position.y) <=
-          circle.radius
-        ) {
-          if (circle.active) continue;
-          circle.active = true;
-          // search for old active circle
-          for (let j = 0; j < this.circles.length; j++) {
-            const tempCircle = this.circles[j];
-            if (tempCircle.data.id == circle.data.id) continue;
-            if (tempCircle.active) tempCircle.active = false;
-          }
-        }
-      }
-    },
     init() {
       // create circles
       for (let i = 0; i < this.proyectos.length; i++) {
-        const minRadius = this.randomIntFromRange(35, 50);
-        const maxRadius = 150;
-        const radius = i == 0 ? maxRadius : minRadius;
-        let x = this.randomIntFromRange(radius, this.width - radius);
-        let y = this.randomIntFromRange(radius, this.height - radius);
-
-        // making sure circles dont overlap
-        for (let j = 0; j < this.circles.length; j++) {
-          if (
-            this.getDistance(
-              x,
-              y,
-              this.circles[j].position.x,
-              this.circles[j].position.y
-            ) <
-            radius + this.circles[j].radius
-          ) {
-            // generate new values
-            x = this.randomIntFromRange(radius, this.width - radius);
-            y = this.randomIntFromRange(radius, this.height - radius);
-
-            // restart the loop
-            j = -1;
-          }
-        }
-        // push new circle
+        const radius = this.randomIntFromRange(20, 30);
+        // distance from center
+        const distance = this.randomIntFromRange(
+          this.radius.min + radius,
+          this.radius.max - radius
+        );
         this.circles.push({
-          data: {
-            ...this.proyectos[i]
+          data: { ...this.proyectos[i] },
+          distance,
+          bounce: {
+            min: distance - radius,
+            max: distance + radius,
+            minToMax: Math.floor(this.randomIntFromRange(1, 2)) % 2 === 0
           },
-          position: {
-            x,
-            y
-          },
-          velocity: {
-            x: this.randomIntFromRange(-1, 1),
-            y: this.randomIntFromRange(-1, 1)
-          },
-          radius,
-          minRadius,
-          maxRadius,
-          active: i == 0,
-          mass: 1
+          radians: this.randomIntFromRange(0, Math.PI * 2),
+          velocity: this.randomIntFromRange(2, 5) / 2000,
+          radius
         });
       }
     },
     // main loop
     animate() {
-      this.context.clearRect(0, 0, this.width, this.height);
+      this.context.clearRect(0, 0, this.size, this.size);
       // do stuff
       this.circles.forEach(p => {
         this.update(p);
@@ -117,8 +72,8 @@ export default {
     draw(circle) {
       this.context.beginPath();
       this.context.arc(
-        circle.position.x,
-        circle.position.y,
+        this.center + circle.distance * Math.cos(circle.radians),
+        this.center + circle.distance * Math.sin(circle.radians),
         circle.radius,
         0,
         Math.PI * 2,
@@ -131,96 +86,14 @@ export default {
       this.context.stroke();
       this.context.closePath();
     },
-    // update
     update(circle) {
-      // update size if necesary
-      if (circle.active) {
-        if (circle.radius < circle.maxRadius) circle.radius++;
-      } else if (circle.radius > circle.minRadius) circle.radius--;
-
-      // movement of each particle
-      if (
-        circle.position.x - circle.radius < 0 ||
-        circle.position.x + circle.radius > this.width
-      )
-        circle.velocity.x *= -1;
-      if (
-        circle.position.y - circle.radius < 0 ||
-        circle.position.y + circle.radius > this.height
-      )
-        circle.velocity.y *= -1;
-      circle.position.x += circle.velocity.x;
-      circle.position.y += circle.velocity.y;
-
-      // validate each circle with the others to detect collision
-      for (let i = 0; i < this.circles.length; i++) {
-        const tempCircle = this.circles[i];
-        if (circle.data.id == tempCircle.data.id) continue;
-
-        // if 2 circles are collisioning then resolve it.
-        if (
-          this.getDistance(
-            circle.position.x,
-            circle.position.y,
-            tempCircle.position.x,
-            tempCircle.position.y
-          ) <=
-          circle.radius + tempCircle.radius
-        )
-          this.resolveCollision(circle, tempCircle);
-      }
-    },
-    resolveCollision(circle1, circle2) {
-      const xVelocityDiff = circle1.velocity.x - circle2.velocity.x;
-      const yVelocityDiff = circle1.velocity.y - circle2.velocity.y;
-
-      const xDist = circle2.position.x - circle1.position.x;
-      const yDist = circle2.position.y - circle1.position.y;
-
-      // Prevent accidental overlap of particles
-      if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-        // Grab angle between the two colliding particles
-        const angle = -Math.atan2(
-          circle2.position.y - circle1.position.y,
-          circle2.position.x - circle1.position.x
-        );
-
-        // Store mass in var for better readability in collision equation
-        const m1 = circle1.mass;
-        const m2 = circle2.mass;
-
-        // Velocity before equation
-        const u1 = this.rotate(circle1.velocity, angle);
-        const u2 = this.rotate(circle2.velocity, angle);
-
-        // Velocity after 1d collision equation
-        const v1 = {
-          x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
-          y: u1.y
-        };
-        const v2 = {
-          x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
-          y: u2.y
-        };
-
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = this.rotate(v1, -angle);
-        const vFinal2 = this.rotate(v2, -angle);
-
-        // Swap particle velocities for realistic bounce effect
-        circle1.velocity.x = vFinal1.x;
-        circle1.velocity.y = vFinal1.y;
-
-        circle2.velocity.x = vFinal2.x;
-        circle2.velocity.y = vFinal2.y;
-      }
-    },
-    rotate(velocity, angle) {
-      // Takes velocities and alters them as if the coordinate system they're on was rotated
-      return {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-      };
+      circle.radians += circle.velocity;
+      // update distance from center
+      circle.distance += circle.bounce.minToMax
+        ? circle.radius / 100
+        : -circle.radius / 100;
+      if (circle.distance > circle.bounce.max) circle.bounce.minToMax = false;
+      if (circle.distance < circle.bounce.min) circle.bounce.minToMax = true;
     },
     // utils
     getAreaColor(area) {
@@ -231,12 +104,18 @@ export default {
       return "#ff00ff";
     },
     randomIntFromRange(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
+      return Math.random() * (max - min + 1) + min;
     },
     getDistance(x1, y1, x2, y2) {
       // pythagoras
       let dx = x2 - x1;
       let dy = y2 - y1;
+      return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    },
+    getDistanceFromCenter(x, y) {
+      // pythagoras
+      let dx = x - this.center;
+      let dy = y - this.center;
       return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
     }
   }
